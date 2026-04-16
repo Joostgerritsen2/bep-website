0599409c4ad18ffd401700e8e8f62929786615d7
Joost Gerritsen <joost@bep.expert>
2026-04-16 15:57:59 +0200

    feat: add MarqueeLogos component

---
+++ src/components/MarqueeLogos.tsx
@@ -0,0 +1,34 @@
+'use client'
+import Image from 'next/image'
+import { useLang } from '@/lib/language'
+
+const logos = [
+  { src: '/images/client-tender-strateeg.png', alt: 'Tender Strateeg' },
+  { src: '/images/client-groningen-seaports.png', alt: 'Groningen Seaports' },
+  { src: '/images/client-sjb-advies.png', alt: 'SJB Advies' },
+  { src: '/images/client-bpz.png', alt: 'BPZ' },
+]
+
+export function MarqueeLogos() {
+  const { t } = useLang()
+
+  return (
+    <section className="marquee-wrap">
+      <span className="marquee-label">{t('Vertrouwd door', 'Trusted by')}</span>
+      <div className="marquee-track">
+        {/* Twee keer voor naadloze loop */}
+        {[...logos, ...logos].map((logo, i) => (
+          <div key={i} className="marquee-logo-item">
+            <Image
+              src={logo.src}
+              alt={logo.alt}
+              width={140}
+              height={40}
+              style={{ objectFit: 'contain' }}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
